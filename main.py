b34183623653cfe8b4f11a514c4f02ad6a4d5e75
Brian Faure <bfaure23@gmail.com>
2017-08-19 18:29:03 -0400

    cleaned up, threw stuff into functions

--- main.py
+++ main.py
@@ -19,79 +19,53 @@ import quandl
 
 #######################################################
 
-'''
-# plots the symbol price over the specified date range
-# Yahoo finance api seems to be discontinued, this function
-# no longer works, we should check out other stock price apis/libraries
-# google I know has one
-def plot_hist_stock_price(symbol='TSLA',start='2014-04-25',end='2014-04-29'):
+# shouldn't really have this on a public repo but fuck it
+def set_api_key(key="X6zvDM1FEEkWU7H1w___"):
+	# register the key (free acct)
+	quandl.ApiConfig.api_key = key
 
-	ticker = Share(symbol) # initialize share
-
-	data = ticker.get_historical(start,end)
-
-	opens 	= [] # open prices, not used
-	closes 	= [] # close prices, not used
-	highs 	= [] # high prices
-	lows 	= [] # low prices, not used
-	volumes = [] # volumes, not used
-
-	for item in data:
-		highs.append(item['High'])
-
-	print highs
-'''
-
-def main():
-
-	# free account key (quandl.com)
-	api_key = "X6zvDM1FEEkWU7H1w___"
-
-	# register the key
-	quandl.ApiConfig.api_key = api_key 
-
+# fetches a data table from quandl (set to Visa, inc. by default)
+def get_data(quandl_code="EOD/V"):
 	# fetch some sample data (Visa, Inc.)
 	data = quandl.get("EOD/V")
+	return data
 
-	# the data object is a 2373 row, 12 column table
 
-	# columns are:
-	# Date, Open, High, Low, Close, Volume, Dividend, Split, \
-	# Adj_Open, Adj_High, Adj_Low, Adj_Close, Adj_Volume
+# all colums of the data tables returned from quandl, not including
+# the first column which is the date (string formatted such as 
+# '2008-03-19')
+valid_table_columes = [	"High","Low","Open","Close","Volume",
+						"Dividend","Split","Adj_Open","Adj_High",
+						"Adj_Low","Adj_Close","Adj_Volume"]
 
-	# each row is for a certain date, dates are structured like:
-	# '2008-03-19'
+# returns a single column of the provided data table
+def get_table_column(data,column="High"):
+	if column not in valid_table_columes: print "You are retarded"
+	col = data[column]
+	return col 
 
-	# putting the data into separate variables...
+# returns the named dates of provided data in list format
+def get_table_dates(data):
+	axes = data.axes 
+	dates = axes[0]
+	return dates
 
-	dates 		= []
-	opens 		= data['High']
-	highs 		= data['Low']
-	lows 		= data['Low']
-	closes 		= data['Close']
-	volumes 	= data['Volume']
-	dividends 	= data['Dividend']
-	splits 		= data['Split']
-	adj_opens 	= data['Adj_Open']
-	adj_highs 	= data['Adj_High']
-	adj_lows 	= data['Adj_Low']
-	adj_closes 	= data['Adj_Close']
-	adj_volumes = data['Adj_Volume']
 
-	# now each of the above variables (opens...adj_volumes) can be
-	# addresses as: opens[0] or opens['2008-03-19'], fill in your own 
-	# index or date
-
-	# putting the opens data into a single list so we can plot...
-
-	plot_opens = []
-	for i in range(len(opens)):
-		plot_opens.append(opens[i])
-
-	plt.plot(plot_opens)
-	plt.ylabel("Visa Open Values")
+# plots the input data
+def plot(xaxis,ylabel="Data"):
+	plt.plot(xaxis)
+	plt.ylabel = ylabel
 	plt.show()
 
+
+def main():
+	set_api_key()
+
+	data = get_data()
+
+	highs = get_table_column(data,"High")
+
+	plot(highs)
 
 
 if __name__ == '__main__':
